42ac251d1daf5be5aeb9e132bd62c7f4cd9af37d
ultratynoz <tynoz@web.de>
2023-05-02 18:44:10 +0200

    finished functionality

--- game.py
+++ game.py
@@ -7,16 +7,15 @@ def get_random_artists():
         data = json.load(fp)
 
         artist1_data = random.choice(data)
-        #artist2_data = random.choice(data)
+        artist2_data = random.choice(data)
 
-        #while artist1_data == artist2_data:
-            # Avoid duplicate selections:
-            #artist2_data = random.choice(data)
+        while artist1_data == artist2_data:
+             #Avoid duplicate selections:
+            artist2_data = random.choice(data)
 
-        print(f"p{artist1_data}")
         artist1 = Artist(artist1_data["name"], artist1_data["followers"], artist1_data["popularity"])
-        #artist2 = Artist(artist2_data[0], artist2_data[1], artist2_data[2])
-        return artist1#, artist2
+        artist2 = Artist(artist2_data["name"], artist2_data["followers"], artist2_data["popularity"])
+        return artist1, artist2
 
 
 
@@ -28,8 +27,10 @@ class Artist:
 
 
 class Game:
-    artists = get_random_artists()
-    score = 0
+
+    def __init__(self):
+        self.artists = get_random_artists()
+        self.score = 0
 
     def get_winner_artist(self):
         if self.artists[0].followers > self.artists[1].followers:
@@ -39,15 +40,15 @@ class Game:
 
     def start_game(self):
         print("Which of the following artists has more followers?")
-        print("1: " + self.one.name)
-        print("2: " + self.two.name)
+        print("1: " + self.artists[0].name)
+        print("2: " + self.artists[1].name)
 
         answer = input()
         while answer != "1" and answer != "2":
             answer = input("Please write either \"1\" or \"2\"")
-        selected = self.a1[(int(answer) - 1)]
+        selected = self.artists[(int(answer)-1)]
 
-        print(f"You selected {selected}")
+        print(f"You selected {selected.name}")
 
         if selected == self.get_winner_artist():
             print("You won!")
@@ -55,12 +56,22 @@ class Game:
             print("Your score is now " + str(self.score))
         else:
             print("Oh no! You lost!")
-            self.score = 0
             print("Your score is now " + str(self.score))
 
         print("Here are the results:")
-        print(f"1: {self.a1[0]} has {self.a1[1]} followers.")
-        print(f"2: {self.a2[0]} has {self.a2[1]} followers.")
+        print(f"1: {self.artists[0].name} has {self.artists[0].followers} followers.")
+        print(f"2: {self.artists[1].name} has {self.artists[1].followers} followers.")
+        print("_____________________________________________________")
+        if selected == self.get_winner_artist():
+            game = Game()
+            game.score = self.score
+            game.start_game()
+
+def run():
+    game = Game()
+    game.start_game()
 
 
-get_random_artists()
+
+
+
